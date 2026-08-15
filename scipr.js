04e3b80e2c41f1cd8a49abbbc0dc21e95bd06d2e
Abdul Latif <abdullatifbusin@gmail.com>
2026-08-15 15:33:29 +0500

    Remove button functionality and add alert

--- scipr.js
+++ scipr.js
@@ -1,6 +1,1 @@
-const button = document.getElementById('btn');
-const message = document.getElementById('message');
-
-button.addEventListener('click', () => {
-  message.textContent = 'Simple code works!';
-});
+alert("Hello, World!");
